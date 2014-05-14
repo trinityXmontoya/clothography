5644adec46ca4aa7decff293a4448a8fd8cce9a1
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-14 19:34:34 -0400

    edit size retrieval methods

--- app/models/size.rb
+++ app/models/size.rb
@@ -4,16 +4,20 @@ class Size < ActiveRecord::Base
 
   has_many :users, through: :size_users
 
-  def self.retrieve_standard_sizes
-    where("name = ?","Standard") + where("name = ?","Petite") + where("name = ?","Plus")
+  def self.standard_sizes
+    where(name: "Standard").where(name: "Petite").where(name: "Plus")
   end
 
-  def self.retrieve_jean_sizes
-    where("name = ?","Standard-Jeans") + where("name = ?","Petite") + where("name = ?","Plus")
+  def self.jean_sizes
+    where(name: "Standard-Jeans").where(name: "Petite").where(name: "Plus")
   end
 
-  def self.retrieve_bottom_sizes
-    where("name = ?","Standard-Bottoms") + where("name = ?","Petite") + where("name = ?","Plus")
+  def self.bottom_sizes
+    where(name:"Standard-Bottoms").where(name: "Petite").where(name: "Plus")
+  end
+
+  def self.shoe_sizes
+    where(name: "Shoe")
   end
 
 end
