e12883e30f6958963e739ff4665dcec09acb513a
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-14 17:20:01 -0400

    inbox displays only the most recent correspondence

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,13 @@ class MessagesController < ApplicationController
 
   def index
     @user = User.find_by(username: params[:user_id])
-    @sent_messages = @user.sent_messages.order(created_at: :asc)
+    # @sent_messages = @user.sent_messages.order(created_at: :asc)
     # # @sent_messages = @user.sent_messages.uniq(&:receiver_id)
     # @sent_messages= Message.group(:sender_id).group(:receiver_id).order(created_at: :asc)
-    @received_messages = @user.received_messages.order(created_at: :asc)
+    @sent_messages = @user.sent_messages.select('DISTINCT ON (receiver_id) *')
+                                               .order('receiver_id, messages.created_at DESC')
+    @received_messages = @user.received_messages.select('DISTINCT ON (sender_id) *')
+                                                 .order('sender_id, messages.created_at DESC')
 
 end
 
