c7e46cd68b32d5fb99b2f4858d286ee9552f4a86
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-14 20:59:37 -0400

    ransack + chosen functioning search form

---
+++ app/assets/javascripts/listing/search_form.js
@@ -0,0 +1,36 @@
+$(document).ready( function(){
+
+   $("#search-form-reset").on("click", function(){
+     $(".search-field").val('')
+   });
+
+  $("#q_category_id_eq").chosen({
+    width: "150px",
+    search_contains: true,
+  });
+
+  $("#q_brand_id_eq").chosen({
+    width: "200px",
+    search_contains: true,
+    allow_single_deselect: true,
+  });
+
+  $("#q_size_id_eq").chosen({
+    width: "100px",
+    search_contains: true,
+    placeholder_text_multiple: "Sizes"
+  });
+
+  $("#q_condition_eq").chosen({
+    width: "150px",
+    search_contains: true,
+    placeholder_text_multiple: "Condition"
+  });
+
+  $("#q_color_id_eq").chosen({
+    width: "100px",
+    search_contains: true,
+    placeholder_text_multiple: "Colors"
+  });
+
+});
