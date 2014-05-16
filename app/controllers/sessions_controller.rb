9aca7cd94192cf4690675ef2ddebe2a4d2fa39d9
trinityXmontoya <trinityXmontoya@gmail.com>
2014-05-16 02:03:28 -0400

    more styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:user_id])
     if @user && @user.validate_token(params[:auth_token])
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Welcome" + @user.username
+      redirect_to search_listings_path, notice: "Welcome" + @user.username
     else
       redirect_to login_path, notice: "Error logging in."
     end
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
     if (@user.email == "")
       redirect_to edit_user_path(@user), notice: "Welcome " + @user.name + ", please confirm your information to complete registration."
     else
-      redirect_to root_path, notice: "Welcome " + @user.username
+      redirect_to search_listings_path, notice: "Welcome " + @user.username
     end
   end
 
